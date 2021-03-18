8ddb78703669461a8b6f9da102782d7dec6f7ec0
dieuanhn <71412021+dieuanhn@users.noreply.github.com>
2021-03-18 18:17:59 -0400

    ??? idk im confused

--- app.js
+++ app.js
@@ -1,63 +1,155 @@
-const questions = [
+const question = document.querySelector("#question");
+const choices = Array.from(document.querySelectorAll(".choice-text"));
+const progressText = document.querySelector("#progressText");
+const scoreText = document.querySelector("#score");
+const progressBarFull = document.querySelector("#progressBarFull");
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let availableQuestions = [];
+let questions = [
   {
     question: "What country is Venice located in?",
-    choices: ["France", "Italy", "Australia", "Japan"],
-    correctAnswer: 1,
+    choice1: "France",
+    choice2: "Italy",
+    choice3: "Australia",
+    choice4: "Japan",
+    correct: 2,
   },
   {
     question: "Who is the main character in Harry Potter?",
-    choices: [
-      "Henry Porter",
-      "Hairy Potter",
-      "Harry Potter",
-      "Henrie Potsticker",
-    ],
-    correctAnswer: 2,
+    choice1: "Henry Porter",
+    choice2: "Hairy Potter",
+    choice3: "Harry Potter",
+    choice4: "Henrie Potsticker",
+    correct: 3,
   },
   {
-    question: "Which of these Avengers aren't dead?",
-    choices: ["Iron Man", "Hawkeye", "Black Widow", "Vision"],
-    correctAnswer: 1,
+    question: "Which of these Avengers isn't dead?",
+    choice1: "Iron Man",
+    choice2: "Hawkeye",
+    choice3: "Black Widow",
+    choice4: "Vision",
+    correct: 2,
   },
   {
     question: "Who is the best spiderman?",
-    choices: [
-      "Tobey Maguire",
-      "Andrew Garfield",
-      "Tom Holland",
-      "none of them",
-    ],
-    correctAnswer: 2,
+    choice1: "Tobey Maguire",
+    choice2: "Andrew Garfield",
+    choice3: "Tom Holland",
+    choice4: "none of them",
+    correct: 3,
   },
   {
     question:
       "Which character from Criminal Minds have suffered the most trauma?",
-    choices: ["Spencer Reid", "JJ", "Aaron Hotch", "Emily Prentis"],
-    correctAnswer: 0,
+    choice1: "Spencer Reid",
+    choice2: "JJ",
+    choice3: "Aaron Hotch",
+    choice4: "Emily Prentis",
+    correct: 1,
   },
   {
     question: "What does mintchip icecream taste like?",
-    choices: [
-      "mint flavored icecream with chocolate",
-      "mint",
-      "chocolate",
-      "toothpaste",
-    ],
-    correctAnswer: 3,
+    choice1: "mint flavored icecream with chocolate",
+    choice2: "mint",
+    choice3: "chocolate",
+    choice4: "toothpaste",
+    correct: 4,
   },
   {
     question: "What is the captial of the US?",
-    choices: ["Washington DC", "Boston", "New York", "San Francisco"],
-    correctAnswer: 0,
+    choice1: "Washington DC",
+    choice2: "Boston",
+    choice3: "New York",
+    choice4: "San Francisco",
+    correct: 1,
+  },
+  {
+    question: "Which of these items can't you write with?",
+    choice1: "pencil",
+    choice2: "carrot",
+    choice3: "pen",
+    choice4: "highlighter",
+    correct: 1,
+  },
+  {
+    question: "Which of these is a vegetable?",
+    choice1: "cows",
+    choice2: "dirt",
+    choice3: "lettuce",
+    choice4: "ice cubes",
+    correct: 3,
   },
   {
     question: "How hard was this quiz?",
-    choices: [
-      "easy",
-      "too easy",
-      "extremely easy",
-      "hardest quiz i've ever taken",
-    ],
-    correctAnswer: 3,
+    choice1: "easy",
+    choice2: "too easy",
+    choice3: "extremely easy",
+    choice4: "hardest quiz i've ever taken",
+    correct: 4,
   },
 ];
+
+const SCORE_POINTS = 100;
+const MAX_QUESTIONS = 10;
+
+startGame = () => {
+  questionCounter = 0;
+  score = 0;
+  availableQuestions = [...questions];
+  getNewQuestion();
+};
+
+getNewQuestion = () => {
+  if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+    localStorage.setItem("mostRecentScore", score);
+
+    return window.location.assign("/end.html");
+  }
+
+  const questionsIndex = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestion = availableQuestions[questionsIndex];
+  question.innerText = currentQuestion.question;
+
+  choices.forEach((choice) => {
+    const number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
+  });
+
+  availableQuestions.splice(questionsIndex, 1);
+
+  acceptingAnswers = true;
+};
+
+choices.forEach((choice) => {
+  choice.addEventListener("click", (e) => {
+    if (!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
+
+    let classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+    if (classToApply === "correct") {
+      incrementScore(SCORE_POINTS);
+    }
+
+    selectedChoice.parentElement.classList.add(classToApply);
+
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
+  });
+});
+
+incrementScore = (num) => {
+  score += num;
+  scoreText.innerText = score;
+};
+
+startGame();
