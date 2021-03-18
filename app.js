4b0fa708c70f94eeae6bcc32fd885250f8c68c05
dieuanhn <71412021+dieuanhn@users.noreply.github.com>
2021-03-18 15:40:41 -0400

    array

---
+++ app.js
@@ -0,0 +1,63 @@
+const questions = [
+  {
+    question: "What country is Venice located in?",
+    choices: ["France", "Italy", "Australia", "Japan"],
+    correctAnswer: 1,
+  },
+  {
+    question: "Who is the main character in Harry Potter?",
+    choices: [
+      "Henry Porter",
+      "Hairy Potter",
+      "Harry Potter",
+      "Henrie Potsticker",
+    ],
+    correctAnswer: 2,
+  },
+  {
+    question: "Which of these Avengers aren't dead?",
+    choices: ["Iron Man", "Hawkeye", "Black Widow", "Vision"],
+    correctAnswer: 1,
+  },
+  {
+    question: "Who is the best spiderman?",
+    choices: [
+      "Tobey Maguire",
+      "Andrew Garfield",
+      "Tom Holland",
+      "none of them",
+    ],
+    correctAnswer: 2,
+  },
+  {
+    question:
+      "Which character from Criminal Minds have suffered the most trauma?",
+    choices: ["Spencer Reid", "JJ", "Aaron Hotch", "Emily Prentis"],
+    correctAnswer: 0,
+  },
+  {
+    question: "What does mintchip icecream taste like?",
+    choices: [
+      "mint flavored icecream with chocolate",
+      "mint",
+      "chocolate",
+      "toothpaste",
+    ],
+    correctAnswer: 3,
+  },
+  {
+    question: "What is the captial of the US?",
+    choices: ["Washington DC", "Boston", "New York", "San Francisco"],
+    correctAnswer: 0,
+  },
+  {
+    question: "How hard was this quiz?",
+    choices: [
+      "easy",
+      "too easy",
+      "extremely easy",
+      "hardest quiz i've ever taken",
+    ],
+    correctAnswer: 3,
+  },
+];
